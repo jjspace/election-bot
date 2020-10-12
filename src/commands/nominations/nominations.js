8008a18834a9e5ae671bf122da44f45dd24d5e1b
jjspace <jjrouzer@yahoo.com>
2020-10-12 11:20:07 -0400

    extracting enums

--- src/commands/nominations/nominations.js
+++ src/commands/nominations/nominations.js
@@ -1,22 +1,7 @@
 const { MessageEmbed } = require('discord.js');
 const dbClient = require('../../db/dbClient');
 const { withServerDB } = require('../commandMods');
-
-// https://birdie0.github.io/discord-webhooks-guide/other/field_limits.html
-const LIMITS = {
-  USERNAME: 32,
-  CONTENT: 2000,
-  EMBEDS: 10,
-  FILE: 10,
-  TITLE: 256,
-  DESCRIPTION: 2048,
-  AUTHOR_NAME: 256,
-  FIELDS: 25,
-  FIELD_NAME: 256,
-  FIELD_VALUE: 1024,
-  FOOTER_TEXT: 2048,
-  SUM_CHAR_IN_EMBED: 6000,
-};
+const { EMBED_LIMITS } = require('../../enums');
 
 const nominations = {
   name: 'nominations',
@@ -33,7 +18,7 @@ const nominations = {
           return `\\+${nomMention} (${count}) – ${nominatorsStr}`;
         })
         .join('\n');
-      if (nomStr.length > LIMITS.FIELD_VALUE) {
+      if (nomStr.length > EMBED_LIMITS.FIELD_VALUE) {
         nomStr = list
           .map((nom) => {
             const nomMention = message.guild.member(nom.userId);
